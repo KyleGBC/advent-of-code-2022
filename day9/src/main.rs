4c11ce31eb736659ccaa19b4721fc01b0bbe8281
KyleGBC <kyleroleson@gmail.com>
2022-12-09 12:40:52 -0500

    Optimized day 9 to 800 microseconds

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,4 +1,8 @@
-fn update_knot(leader: (isize, isize), follower: &mut (isize, isize)) {
+use std::hash::BuildHasherDefault;
+use std::collections::HashSet;
+use hashers::fx_hash::FxHasher;
+
+fn update_knot(leader: (i32, i32), follower: &mut (i32, i32)) {
     let (x_dist, y_dist) = (leader.0 - follower.0, leader.1 - follower.1);
     if x_dist.abs() > 1 || y_dist.abs() > 1 {
         follower.0 += 1 * x_dist.signum();
@@ -6,10 +10,10 @@ fn update_knot(leader: (isize, isize), follower: &mut (isize, isize)) {
     }
 }
 
-fn run_with_n_knots(lines: std::str::Lines, num_knots: usize) -> usize {
-    let mut knot_positions: Vec<(isize, isize)> = Vec::with_capacity(num_knots);
-    knot_positions.resize(num_knots, (0, 0));
-    let mut tail_positions: std::collections::HashSet<(isize, isize)> = std::collections::HashSet::with_capacity(10000);
+fn run_with_n_knots<const N: usize>(lines: std::str::Lines) -> (usize, usize) {
+    let mut knot_positions = [(0_i32, 0_i32); N];
+    let mut tail_positions: HashSet<(i32, i32), BuildHasherDefault<FxHasher>> = HashSet::with_capacity_and_hasher(10000, BuildHasherDefault::<FxHasher>::default());
+    let mut behind_head_positions: HashSet<(i32, i32), BuildHasherDefault<FxHasher>> = HashSet::with_capacity_and_hasher(10000, BuildHasherDefault::<FxHasher>::default());
 
     for line in lines {
         let (dir, step) = line.split_once(' ').unwrap();
@@ -18,22 +22,22 @@ fn run_with_n_knots(lines: std::str::Lines, num_knots: usize) -> usize {
             match dir {
                 "U" => knot_positions[0].1 += 1,
                 "D" => knot_positions[0].1 -= 1,
-                "R" => knot_positions[0].0 += 1,
-                "L" => knot_positions[0].0 -= 1,
+                "L" => knot_positions[0].0 += 1,
+                "R" => knot_positions[0].0 -= 1,
                 _ => unreachable!(),
             }
-            for l in 0..num_knots - 1 {
+            for l in 0..N - 1 {
                 update_knot(knot_positions[l as usize], &mut knot_positions[l + 1 as usize]);
             }
-            tail_positions.insert(knot_positions[num_knots - 1]);
-            
+            tail_positions.insert(knot_positions[N - 1]);
+            behind_head_positions.insert(knot_positions[1]);
         }           
     }
-    tail_positions.len()
+    (behind_head_positions.len(), tail_positions.len())
 }
 
 fn main() {
     let now = std::time::Instant::now();
     let input = std::fs::read_to_string("input.txt").unwrap();
-    println!("Part 1: {}, Part 2: {}, took {:#?}", run_with_n_knots(input.lines(), 2), run_with_n_knots(input.lines(), 10), now.elapsed());
+    println!("{:?} in {:#?}", run_with_n_knots::<10>(input.lines()), now.elapsed());
 }
