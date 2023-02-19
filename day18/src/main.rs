7bc4d1506b5b35de85122f6f5ac4be780935bc7e
KyleGBC <kyleroleson@gmail.com>
2023-02-18 20:08:51 -0500

    Working part2 :D

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,91 +1,111 @@
-use std::collections::{HashSet, VecDeque};
+use std::{collections::{HashMap, HashSet, VecDeque}, time::Instant };
 
-fn adjacent_air(current: (usize, usize, usize), other_cubes: &HashSet<(usize, usize, usize)>) -> Vec<(usize, usize, usize)> {
-    let mut adjacent = Vec::with_capacity(6);
-    if !other_cubes.contains(&(current.0 + 1, current.1, current.2)) { adjacent.push((current.0 + 1, current.1, current.2)) }
-    if !other_cubes.contains(&(current.0, current.1 + 1, current.2)) { adjacent.push((current.0, current.1 + 1, current.2)) }
-    if !other_cubes.contains(&(current.0, current.1, current.2 + 1)) { adjacent.push((current.0, current.1, current.2 + 1)) }
-    if !other_cubes.contains(&(current.0 - 1, current.1, current.2)) { adjacent.push((current.0 - 1, current.1, current.2)) }
-    if !other_cubes.contains(&(current.0, current.1 - 1, current.2)) { adjacent.push((current.0, current.1 - 1, current.2)) }
-    if !other_cubes.contains(&(current.0, current.1, current.2 - 1)) { adjacent.push((current.0, current.1, current.2 - 1)) }
-    adjacent
+#[derive(PartialEq, Eq, Debug)]
+enum Mat { Lava, Air }
+type Coord = (i32, i32, i32);
+
+fn part1(data: &HashMap<Coord, Mat>) -> i32 {
+    let mut total = 0;
+    for (coord, mat) in data {
+        if mat == &Mat::Air { continue }
+
+        let mut surface_area = 6;
+        let (x, y, z) = *coord;
+
+        if data.get(&(x + 1, y, z)) == Some(&Mat::Lava) { surface_area -= 1 }
+        if data.get(&(x - 1, y, z)) == Some(&Mat::Lava) { surface_area -= 1 }
+        if data.get(&(x, y + 1, z)) == Some(&Mat::Lava) { surface_area -= 1 }
+        if data.get(&(x, y - 1, z)) == Some(&Mat::Lava) { surface_area -= 1 }
+        if data.get(&(x, y, z + 1)) == Some(&Mat::Lava) { surface_area -= 1 }
+        if data.get(&(x, y, z - 1)) == Some(&Mat::Lava) { surface_area -= 1 }
+        total += surface_area
+    }
+    total
 }
 
+fn get_adjacent(data: &HashMap<Coord, Mat>, root: Coord) -> Vec<Coord> {
+    let mut ret = Vec::with_capacity(6);
+    let (x, y, z) = root;
 
-fn bfs(start: (usize, usize, usize), goal: (usize, usize, usize), other_cubes: &HashSet<(usize, usize, usize)>) -> HashSet<(usize, usize, usize)> {
-    let mut q: VecDeque<(usize, usize, usize)> = VecDeque::new();
-    let mut explored: HashSet<(usize, usize, usize)> = HashSet::new();
-    explored.insert(start);
+    if data.get(&(x + 1, y, z)) == Some(&Mat::Air) { ret.push((x + 1, y, z)) }
+    if data.get(&(x - 1, y, z)) == Some(&Mat::Air) { ret.push((x - 1, y, z)) }
+    if data.get(&(x, y + 1, z)) == Some(&Mat::Air) { ret.push((x, y + 1, z)) }
+    if data.get(&(x, y - 1, z)) == Some(&Mat::Air) { ret.push((x, y - 1, z)) }
+    if data.get(&(x, y, z + 1)) == Some(&Mat::Air) { ret.push((x, y, z + 1)) }
+    if data.get(&(x, y, z - 1)) == Some(&Mat::Air) { ret.push((x, y, z - 1)) }
 
-    while !q.is_empty() {
-        let v = q.pop_front().unwrap();
-        for w in adjacent_air(v, other_cubes) {
-            if !explored.contains(&w) {
-                explored.insert(w);
-                q.push_back(w)
+    ret
+}
+
+fn is_exterior_air(data: &HashMap<Coord, Mat>, root: Coord) -> bool {
+    if data.get(&root) == Some(&Mat::Lava) { return false }
+
+    let mut exploration: HashSet<Coord> = HashSet::with_capacity(20_000);
+    let mut queue: VecDeque<Coord> = VecDeque::with_capacity(20_000);
+
+    exploration.insert(root);
+    queue.push_back(root);
+    while !queue.is_empty() {   
+        let v = queue.pop_front().unwrap();
+        if v == (0, 0, 0) {
+            return true;
+        }
+        for adj in get_adjacent(data, v) {
+            let adj = adj;
+            if !exploration.contains(&adj) {
+                exploration.insert(adj);
+                queue.push_back(adj)
             }
         }
     }
-    return explored;
+    false
 }
 
-fn surface_area(cube: (usize, usize, usize), other_cubes: &HashSet<(usize, usize, usize)>) -> usize {
-    let mut surface_area = 6;
-    if other_cubes.contains(&(cube.0 + 1, cube.1, cube.2)) { surface_area -= 1 }
-    if other_cubes.contains(&(cube.0, cube.1 + 1, cube.2)) { surface_area -= 1 }
-    if other_cubes.contains(&(cube.0, cube.1, cube.2 + 1)) { surface_area -= 1 }
-    
-    if let Some(n) = cube.0.checked_sub(1) {
-        if other_cubes.contains(&(n, cube.1, cube.2)) { surface_area -= 1 }
+fn part2(data: &HashMap<Coord, Mat>) -> i32 {
+    let mut total = 0;
+    for (coord, mat) in data {
+        if mat == &Mat::Air { continue }
+
+        let mut surface_area = 0;
+        let (x, y, z) = *coord;
+
+        if is_exterior_air(data, (x + 1, y, z)) { surface_area += 1 }
+        if is_exterior_air(data, (x - 1, y, z)) { surface_area += 1 }
+        if is_exterior_air(data, (x, y + 1, z)) { surface_area += 1 }
+        if is_exterior_air(data, (x, y - 1, z)) { surface_area += 1 }
+        if is_exterior_air(data, (x, y, z + 1)) { surface_area += 1 }
+        if is_exterior_air(data, (x, y, z - 1)) { surface_area += 1 }
+        total += surface_area;
     }
-    if let Some(n) = cube.1.checked_sub(1) {
-        if other_cubes.contains(&(cube.0, n, cube.2)) { surface_area -= 1 }
-    }
-    if let Some(n) = cube.2.checked_sub(1) {
-        if other_cubes.contains(&(cube.0, cube.1, n)) { surface_area -= 1 }
-    }
-    surface_area
+    total
 }
 
 fn main() {
-    let now = std::time::Instant::now();
-    let input = include_str!("../sample.txt");
-    let mut cubes: HashSet<(usize, usize, usize)> = HashSet::with_capacity(2025);
-    let (mut max_x, mut max_y, mut max_z) = (0, 0, 0);
-    let (mut min_x, mut min_y, mut min_z) = (usize::MAX, usize::MAX, usize::MAX);
-    let mut max_y = 0;
-    let mut max_z = 0;
-    let mut min_x = usize::MAX;
-    let mut min_z = usize::MAX;
-    let mut min_y = usize::MAX;
-    for line in input.lines() {
-        let mut pos = line.split(',');
-        let (x, y, z) = ( pos.next().unwrap().parse::<usize>().unwrap(),
-                                           pos.next().unwrap().parse::<usize>().unwrap(),
-                                           pos.next().unwrap().parse::<usize>().unwrap() );
-        max_x = usize::max(max_x, x);
-        max_y = usize::max(max_y, y);
-        max_z = usize::max(max_z, z);
-        min_x = usize::min(min_x, x);
-        min_y = usize::min(min_y, y);
-        min_z = usize::min(min_z, z);
-        cubes.insert((x, y, z));
-    }
+    let time = Instant::now();
+    let input = include_str!("../input.txt");
+    let mut data: HashMap<Coord, Mat> = HashMap::with_capacity(100);
 
-    let mut filled = cubes.clone();
-    let max_x = 
-    for x in 1..20 {
-        for y in 1..20 {
-            for z in 1..20 {
-                if !cubes.contains(&(x, y, z)) {
-                    println!("Air pocket found at {},{},{}", x,y, z);
-                    filled.insert((x, y, z));
-                }
+    // Get all the lava points
+    for line in input.lines() {
+        let v: Vec<_> = line.split(',').collect();
+        data.insert((v[0].parse::<i32>().unwrap(), v[1].parse::<i32>().unwrap(), v[2].parse::<i32>().unwrap()), Mat::Lava);
+    }
+    
+    // Find the bounds of the space
+    let max_x = data.iter().map(|((x, _, _), _)| *x).max().unwrap();
+    let max_y = data.iter().map(|((_, y, _), _)| *y).max().unwrap();
+    let max_z = data.iter().map(|((_, _, z), _)| *z).max().unwrap();
+
+    // Tag the remaining parts as Air
+    for x in -1..=(max_x+1) {
+        for y in -1..=(max_y+1) {
+            for z in -1..=(max_z+1) {
+                if !data.contains_key(&(x, y, z)) { data.insert((x, y, z), Mat::Air); }
             }
         }
     }
 
-    let part1 = cubes.iter().fold(0, |acc, c| acc + surface_area(*c, &cubes));
-    let part2 = filled.iter().fold(0, |acc, c| acc + surface_area(*c, &filled));
-    println!("Part 1: {part1}, Part 2: {part2}, in {:#?}", now.elapsed())
+    let sa = part1(&data);
+    let true_sa = part2(&data);
+    println!("Part 1 is {}, Part 2 is {}, in {:#?}", sa, true_sa, time.elapsed());
 }
