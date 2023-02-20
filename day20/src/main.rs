bc22009412d54acc71a31b87a3afd06592ad55ee
KyleGBC <kyleroleson@gmail.com>
2023-02-20 16:40:08 -0500

    Working day 20

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,45 +1,28 @@
-use std::io::prelude::*;
-#[derive(Clone, Debug)]
-struct OrderedI32 { num: i32, index: i32 }
+//based on a solution by /u/SuperSmurfen
+fn decrypt(nums: Vec<i128>, key: i128, times_mixed: i32) -> i128 {
+    let nums = nums.into_iter().map(|n| n * key).collect::<Vec<_>>();
+    let mut indices = (0..nums.len()).collect::<Vec<_>>();
+
+    for _ in 0..times_mixed {
+        for (i, &shift) in nums.iter().enumerate() {
+            let curr_index = indices.iter().position(|n| *n == i).unwrap();
+            indices.remove(curr_index);
+            indices.insert(i128::rem_euclid(curr_index as i128 + shift, indices.len() as i128) as usize, i);
+        }
+    }
+    let mixed = indices.iter().map(|&n| nums[n]).collect::<Vec<_>>();
+    let anchor = mixed.iter().position(|&n| n == 0).unwrap();
+    (1..=3).map(|n| mixed[(n*1000 + anchor).rem_euclid(mixed.len())]).sum()
+}
+
 
 fn main() {
     let now = std::time::Instant::now();
     let input = include_str!("../input.txt");
-    let length = input.lines().count(); 
-    
-    let mut data: Vec<OrderedI32> = Vec::with_capacity(length);
-    for (index, num) in input.lines().enumerate() {
-        let num = num.parse::<i32>().unwrap();
-        data.push(OrderedI32 { num, index: index as i32 })
-    }
+    let numbers = input.lines().map(|s| s.parse::<i128>().unwrap()).collect::<Vec<_>>();
 
-    for i in 0..length {
-        let curr_idx = data[i].index;
-        let j = i32::rem_euclid(curr_idx + data[i].num, length as i32);
-        let k = i32::rem_euclid(curr_idx + data[i].num +  i32::signum(data[i].num), length as i32);
-        let (l, u) = if j < k { (j, k) } else { (k, j) };
-        let new_idx = if l != 0 {
-            if curr_idx > u { l } else { u }
-        }
-        else {
-            if data[i].num < 0 { u } else { l }
-        };
+    let part1 = decrypt(numbers.clone(), 1, 1);
+    let part2 = decrypt(numbers, 811589153, 10);
 
-        if new_idx < curr_idx {
-            data.iter_mut().filter(|n| n.index < curr_idx && n.index >= new_idx).for_each(|n| n.index += 1);
-            data[i].index = new_idx;
-        }
-        else {
-            data.iter_mut().filter(|n| n.index > curr_idx && n.index <= new_idx).for_each(|n| n.index -= 1);
-            data[i].index = new_idx;
-        }
-
-
-
-        let _ = std::io::stdin().read(&mut [0u8]);
-    }
-    let anchor = data.iter().find(|o| o.num == 0).unwrap().index;
-    let part1: i32 = (1..=3).map(|n| data.iter().find(|o| o.index == (anchor + n*1000) % length as i32).unwrap().num).sum();
-    println!("Part 1: {part1}, Part 2: , in {:#?}", now.elapsed());
-
+    println!("Part 1: {part1}, Part 2: {part2}, in {:#?}", now.elapsed());
 }
